340bb88afe5b04335fc8d2e353a03905995261c3
Warren Henning <warren.henning@gmail.com>
2014-06-07 04:49:06 -0700

    Reformat

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var connect = require('connect');
 
 connect.createServer(
-	connect.static("app")
+    connect.static("app")
 ).listen(5000);
 
 console.log("Now serving at http://localhost:5000");
